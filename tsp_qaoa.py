428f14499af3e641395f9b081364a60ec6f092bd
PabloMartinezAngerosa <pablomartinezangerosa@gmail.com>
2021-07-28 19:58:28 -0300

    Add archivo base

--- tsp_qaoa.py
+++ tsp_qaoa.py
@@ -1,1 +1,20 @@
 import qiskit
+from thirdParty.classical import rand_graph, classical, bitstring_to_path, calc_cost
+
+
+if __name__ == '__main__':
+    
+    cantidad_ciudades = 4
+    pesos, conexiones = None, None
+    mejor_camino = None
+
+    while not mejor_camino:
+        pesos, conexiones = rand_graph(cantidad_ciudades)
+        mejor_costo, mejor_camino = classical(pesos, conexiones, loop=False)
+
+    print(pesos)
+    print(conexiones)
+    print(mejor_camino)
+    print(mejor_costo)
+    
+
